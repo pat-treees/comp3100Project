38d73d531d9bb78db9b0e189a65c653d9f4edf1e
Pat-treees <patricepascual@students.mq.edu.au>
2023-03-26 18:13:30 +1100

    added rcv and send functions

--- TCPClient.java
+++ TCPClient.java
@@ -19,102 +19,89 @@ public class TCPClient {
                 Socket s = new Socket(aHost, aPort);
                 DataOutputStream dout = new DataOutputStream(s.getOutputStream());
                 BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
-
+    
                 System.out.println("Target IP: " + s.getInetAddress() + "Target Port: "+ s.getPort());
                 System.out.println("Local IP: " + s.getLocalAddress() + "Local Port: " + s.getLocalPort());
-
-                dout.write(("HELO\n").getBytes());
-                dout.flush();
-                System.out.println("SENT: HELO");
- 
-                String str = (String)din.readLine();
-                System.out.println("RVCD: " + str);
-
-                dout.write(("AUTH 46370633\n").getBytes());
-                dout.flush();
-                System.out.println("SENT: AUTH 46370633");
-
-                str = (String)din.readLine();
-                System.out.println("RVCD: " + str);
+    
+                sendMessage("HELO", dout);
+    
+                receiveMessage(din);
+    
+                sendMessage("AUTH 46370633", dout);
+    
+                receiveMessage(din);
                 
-                dout.write(("REDY\n").getBytes());
-                dout.flush();
-                System.out.println("SENT: REDY");
+                sendMessage("REDY", dout);
              
-                str = (String)din.readLine();
-                System.out.println("RVCD: " + str);
-
+                String str = receiveMessage(din);
+    
                 String[] jobInfo = str.split(" ");
                 int jobID = Integer.parseInt(jobInfo[2]); // get job ID which is JOBN jobID YY
                 System.out.println("jobID: " + jobID);
                 
-                dout.write(("GETS All\n").getBytes()); // get data 
-                dout.flush();
-                System.out.println("SENT: GETS All");
+                sendMessage("GETS All", dout);
                
-                str = (String)din.readLine();
-                System.out.println("RVCD: " + str);
-
-                dout.write(("OK\n").getBytes());
-                dout.flush();
-                System.out.println("SENT: OK");
+                str = receiveMessage(din);
+    
+                sendMessage("OK", dout);
             
                 String[] serverInfoList = str.split(" ");
-
+    
                 int serverNumber = Integer.parseInt(serverInfoList[1]); //get the X (server type)
                 System.out.println("Server number: " + serverNumber);
-
+    
                 for (int i = 0; i < serverNumber; i++){
-                    str = (String)din.readLine();
-                    System.out.println("RVCD: " + str);
+                   str = receiveMessage(din);
                  //loop x times and by the x time, you get the largest server type and server ID
                 }
-
+    
+                //extract ServerType and ServerID from data presented by server 
                 String[] serverData = str.split(" ");
                 String serverType = serverData[0];
                 int serverID = Integer.parseInt(serverData[1]);
-
-                dout.write(("OK\n").getBytes());
-                dout.flush();
-                System.out.println("SENT: OK");
-
-                str = (String)din.readLine();
-                System.out.println("RVCD: " + str);
-
+    
+                sendMessage("OK", dout);
+    
+                receiveMessage(din);
+    
                 System.out.println("SCHD " + jobID + " " + serverType + " " + serverID +"\n");
-
+    
                //SCHD 1 job 
                 dout.write(("SCHD " + jobID + " " + serverType + " " + serverID +"\n").getBytes());
                 dout.flush();
             
-                str = (String)din.readLine();
-                System.out.println("RVCD: " + str);
-
-                dout.write(("QUIT\n").getBytes());
-                dout.flush();
-                System.out.println("SENT: QUIT");
-
+                receiveMessage(din);
+    
+                sendMessage("QUIT", dout);
+    
                 
                 din.close();
                 dout.close();
                 s.close();
             
                 }
-
+    
             catch(Exception e){System.out.println(e);
             }
          
+         
+        }
+    
+        // Function below sends message to server and prints it out on terminal
+        static void sendMessage(String str, DataOutputStream dout) throws IOException{ 
+            dout.write((str + "\n").getBytes());
+            dout.flush();
+            System.out.println("SENT: " + str);
+        }
+    
+        // Function below receivers message from server and prints it out on terminal
+        static String receiveMessage(BufferedReader din) throws IOException{
+            String str = (String)din.readLine();
+            System.out.println("RVCD: " + str);
+            return str;
+    
         }
 
-}
-
-
-
-
-            
-
-           
-            
-
+ }
     
-
+    
