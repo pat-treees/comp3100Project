ff478b276cbd1fe91d47ece7ecd739c5f8bfed20
Patrice-Pascual <79579720+Pat-treees@users.noreply.github.com>
2023-05-24 22:13:43 +1000

    Made Client java file for assignment 2

---
+++ Ass2Client.java
@@ -0,0 +1,154 @@
+import java.net.Socket;
+import java.net.InetAddress;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.util.*;
+
+public class Ass2Client {
+    /**
+     *  * @param args
+     *  */
+        public static void main(String[] args) {
+            try {
+                InetAddress aHost = InetAddress.getByName(args[0]);
+                int aPort = Integer.parseInt(args[1]);
+                Socket s = new Socket(aHost, aPort);
+                DataOutputStream dout = new DataOutputStream(s.getOutputStream());
+                         BufferedReader din = new BufferedReader(new InputStreamReader(s.getInputStream()));
+                
+                System.out.println("Target IP: " + s.getInetAddress() + "Target Port: "+ s.getPort());
+                      System.out.println("Local IP: " + s.getLocalAddress() + "Local Port: " + s.getLocalPort());
+                String username = System.getProperty("user.name");
+
+                sendMessage("HELO", dout);
+    
+                receiveMessage(din); //RCVD OK
+    
+                sendMessage("AUTH " + username, dout);
+    
+                receiveMessage(din); // RCVD OK
+
+                
+                int serverID = 0;
+                String serverType = " ";
+                
+                while(true){ 
+                    
+                    sendMessage("REDY", dout);
+             
+                    //-receiving job data and adding it on to string
+                    String jobInfo = receiveMessage(din);
+                    String[] jobs = jobInfo.split(" "); // RVCD JOB INFO
+                    
+                    if(jobs[0].equals("NONE")){ 
+                        break;
+                    }
+
+                    if(jobs[0].equals("JOBN")){
+
+                    int jobID = Integer.parseInt(jobs[2]); // get job ID which is JOBN jobID YY
+
+                    int jobCore = Integer.parseInt(jobs[4]);
+                    int jobRAM = Integer.parseInt(jobs[5]);
+                    int jobDisk = Integer.parseInt(jobs[6]);
+                    int flag = 1;
+                    sendMessage("GETS Avail " + jobCore + " " + jobRAM + " " + jobDisk, dout);
+
+                    String str = receiveMessage(din); // RCV full server DATA
+                
+                    String[] serverInfoList = str.split(" ");
+    
+                    int serverNumber = Integer.parseInt(serverInfoList[1]); //get the X (server type)
+            
+                    if(serverNumber == 0){
+                        sendMessage("OK", dout);
+                        receiveMessage(din); // RCV '.' 
+                        sendMessage("GETS Capable " + jobCore + " " + jobRAM + " " + jobDisk, dout);
+                        str = receiveMessage(din); 
+
+                        sendMessage("OK", dout);
+
+                        serverInfoList = str.split(" ");
+
+                        serverNumber = Integer.parseInt(serverInfoList[1]); //get the X (server type)
+
+                    
+                     //record all available servers one by one
+                        for(int i = 0; i < serverNumber; i++){
+                         //loop x times and by the x time, you get the largest server type and server ID
+                        str = receiveMessage(din); //DATA outputed one by one 
+                        String[] serverData = str.split(" ");
+                        if (flag == 1){
+                                serverID = Integer.parseInt(serverData[1]);  
+                                serverType = serverData[0];                                  
+                        }
+                       flag = 0;
+                        }
+
+                        flag = 1;
+
+                    } else {
+
+                        sendMessage("OK", dout);
+
+                     //record all available servers one by one
+                        for(int i = 0; i < serverNumber; i++){
+                         //loop x times and by the x time, you get the largest server type and server ID
+                        str = receiveMessage(din); //DATA outputed one by one 
+                        String[] serverData = str.split(" ");
+                        if (flag == 1){
+                                serverID = Integer.parseInt(serverData[1]);  
+                                serverType = serverData[0];                                  
+                        }
+                       flag = 0;
+                        }
+
+                        flag = 1;
+
+                }
+
+    
+
+                    sendMessage("OK", dout);
+    
+                    receiveMessage(din); //RCV " . "
+
+                    //SCHD 1 job if JOBN
+                        dout.write(("SCHD " + jobID + " " + serverType + " " + serverID +"\n").getBytes());
+                        System.out.println("SCHD " + jobID + " " + serverType + " " + serverID +"\n");
+                        dout.flush();
+                        receiveMessage(din); 
+
+        
+                }
+                }
+    
+                sendMessage("QUIT", dout);
+    
+                din.close();
+                dout.close();
+                s.close();
+            
+            } 
+
+
+            catch(Exception e){System.out.println(e);
+            }
+         
+        }
+
+    // Function below sends message to server and prints it out on terminal
+    static void sendMessage(String str, DataOutputStream dout) throws IOException {
+        dout.write((str + "\n").getBytes());
+        dout.flush();
+    }
+
+    // Function below receivers message from server and prints it out on terminal
+    static String receiveMessage(BufferedReader din) throws IOException {
+        String str = (String) din.readLine();
+        return str;
+
+    }
+        }
